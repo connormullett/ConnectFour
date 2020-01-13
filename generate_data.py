79b44433c0c1eab3c52af72bde32ac0213a416e5
connor <connormullett@gmail.com>
2020-01-13 05:03:44 -0500

    torch dataloader working

--- generate_data.py
+++ generate_data.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import torch
+import numpy as np
+
 from torch.utils.data import DataLoader, Dataset
 
 
@@ -11,26 +13,16 @@ class ConnectDataset(Dataset):
   # class for use with CLEAN, PROCESSED data
 
   def __init__(self):
-    # 42 features and 1 class (win/loss)
-    # need to convert each board to
-    # 3 channels (piece locations)
-    # 6 rows and 7 columns
-
-    # this will have to change to prcessed data dir
-    self.frame = pd.read_csv('./processed/boards.npy')
+    self.boards = np.load('./processed/boards.npy')
 
   def __len__(self):
     return len(self.frame)
   
   def __getitem__(self, idx):
-    if torch.is_tensor(idx):
-      idx = idx.tolist()
-    row = self.frame.iloc[idx, 1:]
-    return row
+    return self.boards[idx]
 
 
 if __name__ == '__main__':
   data = ConnectDataset()
-  
   print(data[0])
 
